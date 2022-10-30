06e1881c866eb4ed9a1de6f74ec033ea7de5db6d
Anne Bortoli <annebortoli@gmail.com>
2022-10-30 13:36:59 -0300

    :construction: create static components

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Venda</td>
+            <td>13/04/2021</td>
+          </tr>
+
+          <tr>
+            <td>Pizza</td>
+            <td className="withdraw">- R$ 89,00</td>
+            <td>Alimentação</td>
+            <td>13/04/2021</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel do Apartamento</td>
+            <td className="withdraw">- R$ 3.500</td>
+            <td>Casa</td>
+            <td>05/05/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
